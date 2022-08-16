be8bd79aab297844b7394ccfa6a2dcf3d700a6fc
aboongm <aboongm@yahoo.com>
2022-08-16 15:11:13 +0530

    Update the greetings api routes and create the greetings controller in api/v1 folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api, default: { format: 'json'} do
+    namespace :v1 do
+      get 'greetings' => "greetings#index"
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
